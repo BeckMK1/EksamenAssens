9f4183918175a0109e91b219d40a5a17299cb441
oskar <oskar.wiegaard@hotmail.com>
2019-12-10 10:29:51 +0100

    Hoverpicture fixed
    Hoverpicture fixed

--- js/script.js
+++ js/script.js
@@ -9,11 +9,12 @@
 		$(this).toggleClass('open');
     })
 
-    $('.facilitiesheader1').hover(function() {
-    $(".facilitiesheader1").addClass('transition');
+
+    $('.facilitiesheader').hover(function() {
+    $("#mainpic").addClass('transition');
 
 }, function() {
-    $(".facilitiesheader1").removeClass('transition');
+    $("#mainpic").removeClass('transition');
 });
 
     })
